e2ed5d81bc8401e9964435f67193b88058fb1fbc
Aaron Planell <aaronplanell@gmail.com>
2018-09-21 13:36:04 +0200

    DetailInfoHeader resposive for a long name movie

--- src/components/Detail/DetailInfoHeader.js
+++ src/components/Detail/DetailInfoHeader.js
@@ -1,28 +1,25 @@
-import React, { PureComponent, Fragment } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+
+import './DetailInfoHeader.css';
 
 class DetailInfoHeader extends PureComponent {
   getTagline = tagline => {
     if (!tagline || tagline === '') return null;
     else {
-      return (
-        <h4 style={{ marginLeft: '10px', fontStyle: 'oblique' }}>
-          {' '}
-          — {tagline}
-        </h4>
-      );
+      return <h4 className="DetailInfoHeaderTagline"> — {tagline}</h4>;
     }
   };
 
   render() {
     const { title, tagline } = this.props;
     return (
-      <Fragment>
+      <div className="DetailInfoHeader">
         <h2>
           <strong>{title}</strong>
         </h2>
         {this.getTagline(tagline)}
-      </Fragment>
+      </div>
     );
   }
 }
